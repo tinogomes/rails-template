946e6d3afc1a1f47e6292ce562d0fb8a30cad9e5
Celestino Gomes <tinorj@gmail.com>
2012-06-04 15:30:50 -0300

    Update master

--- base.rb
+++ base.rb
@@ -4,14 +4,14 @@ git :init
  
 run "echo 'TODO add readme content' > README"
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
-run "cp config/database.yml config/example_database.yml"
+run "cp config/database.yml config/database.yml.sample"
 run "rm public/images/rails.png"
 run "rm public/index.html"
  
 file ".gitignore", <<-GITIGNORE
 log/*.log
 tmp/**/*
-config/database.yml
+config/*.yml
 db/*.sqlite3
 GITIGNORE
  
