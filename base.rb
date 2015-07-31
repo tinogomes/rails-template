7a5ff32eb2f803905a9306548e4f3445e07d328a
tinogomes <tinorj@gmail.com>
2015-07-31 03:08:01 -0300

    refactoring to Rails 4

--- base.rb
+++ base.rb
@@ -5,100 +5,113 @@ if options['skip_gemfile']
   exit 1
 end
 
-run "/bin/bash -lc 'rvm #{ENV['RUBY_VERSION']}@#{app_name} --create --ruby-version'"
+if options['skip_git']
+  puts 'R U MAD? NO GIT? GET OUT OF HERE!'
+  exit 1
+end
 
 def remove_comments_for(filename)
   gsub_file filename, /^\s*#.*\n/, ''
 end
 
-def comment_line_on(filename, expression)
-  gsub_file filename, Regexp.new("^.*#{expression}.*$"), '# \0'
+if yes?("Do you want to create a RVM gemset for #{app_name}")
+  run "/bin/bash -lc 'rvm #{ENV['RUBY_VERSION']}@#{app_name} --create --ruby-version'"
 end
 
-remove_comments_for 'Gemfile'
-comment_line_on 'Gemfile', 'coffee'
-comment_line_on 'Gemfile', 'turbolinks'
+git :init
 
-gem 'unicorn-rails'
+append_file '.gitignore', '/.ruby-*'
+append_file '.gitignore', '/config/*.yml'
+
+git add: '.'
+git commit: '-m "Rails app base"'
+
+uncomment_lines 'Gemfile', 'bcrypt' if yes?('Use BCrypt?')
+uncomment_lines 'Gemfile', 'unicorn' if yes?('Use Unicorn?')
+uncomment_lines 'Gemfile', 'capistrano' if yes?('Use Capistrano?')
+comment_lines 'Gemfile', 'coffee'
+remove_comments_for 'Gemfile'
 
 gem_group :development do
   gem 'brakeman'
+  gem 'foreman'
+  gem 'mailcatcher'
   gem 'rubocop'
 end
 
 gem_group :development, :test do
   gem 'dotenv-rails'
   gem 'guard-rspec'
-  gem 'pry-debugger'
+  gem 'byebug'
   gem 'rspec-rails'
 end
 
-remove_comments_for 'config/routes.rb'
+run 'bundle install'
 
-create_file 'README.mkdn', <<-README
-# #{app_name}
+git add: '.'
+git commit: '-m "gems installed"'
 
-TBD
-
-README
-
-initializer 'secret_token.rb', <<-CODE
-if ENV['SECRET_TOKEN'].nil? || ENV['SECRET_TOKEN'].empty?
-  warn('SECRET_TOKEN is not defined. Try `export SECRET_TOKEN=$(rake secret)`')
-  exit 1
-end
-
-#{app_const_base}::Application.config.secret_key_base = ENV['SECRET_TOKEN']
-
-CODE
-
-create_file '.env', "SECRET_TOKEN=#{app_secret}"
-
-remove_file "README.rdoc"
-
-run 'bundle'
+create_file '.env', "SECRET_KEY_BASE=#{`rake secret`}"
 
 generate 'rspec:install'
-get 'https://gist.github.com/tinogomes/6082570/raw/e7cb366b0376a594d3928a8bb744e4a154e671e8/.rspec', '.rspec'
 
-run "guard init rspec"
+create_file '.rspec', <<-RSPEC_OPTIONS
+--color
+--require spec_helper
+--format doc
+--profile
+RSPEC_OPTIONS
+
+run 'guard init rspec'
 
 gsub_file 'Guardfile', 'guard :rspec do', <<-EOF
 rspec_options = {
-  all_after_pass: false,
-  all_on_start: false
+  notification: false
 }
 
 guard :rspec, options: rspec_options do
 EOF
 
+environment 'config.action_mailer.delivery_method = :smtp', env: 'development'
+environment 'config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }', env: 'development'
+
+create_file 'Procfile.development', <<-PROCFILE
+#worker: sidekiq
+#postgres: postgres -D /usr/local/var/postgres
+#elastichsearch: elasticsearch --config=/usr/local/opt/elasticsearch/config/elasticsearch.yml
+#redis: redis-server /usr/local/etc/redis.conf
+mailcatcher: mailcatcher --foreground --verbose
+#web: rails server
+PROCFILE
+
+git add: '.'
+git commit: '-m "Setup development and test gems"'
+
 if yes?('Do you want Home controller?')
-  generate :controller, :home, :index
+  generate :controller, :home, 'index --no-view-specs --skip-routes --no-helper --no-assets'
 
   route "root to: 'home#index'"
+
+  git add: '.'
+  git commit: '-m "Home controller created!"'
 end
 
-if !options['skip_active_record']
-  run 'cp config/database.yml config/database.yml.sample'
-
-  rake 'db:create:all'
+unless options['skip_active_record']
+  rake 'db:create'
   rake 'db:migrate'
-  rake 'db:test:prepare'
-else
-  remove_file 'config/database.yml'
 end
 
 rake 'spec'
 
-if !options['skip_git']
-  append_file '.gitignore', '/.ruby-*'
-  append_file '.gitignore', '/config/*.yml'
-
-  git :init
+if yes?('Remove comments for routes file?')
+  remove_comments_for 'config/routes.rb'
   git add: '.'
-  git commit: '-m "It\'s time to get fun!"'
-else
-  puts 'You are not using Git. O_o'
+  git commit: '-m "Removed routes comments"'
 end
 
-puts 'Remember, you should create the SECRET_TOKEN variable for production.'
+puts 'TODO:'
+puts '- Edit README file'
+puts '- Verify bin/setup file'
+puts '- Verify Procfile.development file'
+puts '- Verify .env file'
+puts '- Go work!'
